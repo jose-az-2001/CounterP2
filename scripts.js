7f0302db4256789e10d5d076de203be938f392fe
Jose Daniel Az <josedanielxela@gmail.com>
2022-10-18 23:23:38 -0600

    counter script

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
+var contador = 0
+var divContador = document.getElementById("count")
 function Inc(){
-    
+    contador++
+    divContador.innerText = contador
 }
 
 function Dec(){
-
+    contador--
+    divContador.innerText = contador
 }
 
 function Reset(){
-
+    contador = 0
+    divContador.innerText = contador
 }
